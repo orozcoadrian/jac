7d186782fd8c2fe092ed6d8457701879d4b85294
adrian orozco <orozcoadrian@gmail.com>
2014-06-11 07:41:47 -0400

    added unit test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import unittest
 from bs4 import BeautifulSoup
+import pprint
 
 class Test1(unittest.TestCase):
     def test_one(self):
@@ -61,18 +62,47 @@ class Test1(unittest.TestCase):
                 <td class=oddrows>06/11/2014</td>
                 </tr>
                 <tr>
-                <td class=oddrows>05-2012-CA-062010-XXXX-XX</td>
-                <td class=oddrows>BANK NEW YORK VS ROBERT CASTRO</td>
-                <td class=oddrows>&nbsp;</td>
-                <td class=oddrows>11/05/2014</td>
+                <td class=evenrows>05-2009-CA-064588-XXXX-XX</td>
+                <td class=evenrows>BANK NEW YORK VS JORGE VASQUEZ</td>
+                <td class=evenrows>CANCELLED</td>
+                <td class=evenrows>06/11/2014</td>
                 </tr>
                 </table>
                 </body>
                 </html>"""
-        soup = BeautifulSoup(the_html)
-        print(soup.prettify())
-        # assert_equals()
+        rows=get_rows(the_html)
+        # pprint.pprint(rows)
+        self.assertEqual(len(rows),2)
+        row=rows[0]
+        self.assertEqual(row['case_number'],'05-2008-CA-019393-XXXX-XX')
+        self.assertEqual(row['case_title'],'US BANK VS ALICIA RODRIGUEZ')
+        self.assertEqual(row['comment'],'')
+        self.assertEqual(row['foreclosure_sale_date'],'06/11/2014')
+        row=rows[1]
+        self.assertEqual(row['case_number'],'05-2009-CA-064588-XXXX-XX')
+        self.assertEqual(row['case_title'],'BANK NEW YORK VS JORGE VASQUEZ')
+        self.assertEqual(row['comment'],'CANCELLED')
+        self.assertEqual(row['foreclosure_sale_date'],'06/11/2014')
 
+def get_rows(the_html):
+    rows=[]
+    soup = BeautifulSoup(the_html)
+    # print(soup.prettify())
+    trs=soup.find_all("tr")
+    for tr in trs:
+        # print(tr)
+        current_row = {}
+        tds = tr.find_all('td')
+        if len(tds) == 0:
+            continue
+        # print(tds)
+        current_row['case_number']=tds[0].string
+        current_row['case_title']=tds[1].string
+        current_row['comment']=tds[2].string.replace(u'\xa0', u'').encode('utf-8')
+        current_row['foreclosure_sale_date']=tds[3].string
+        rows.append(current_row)
+    # pprint.pprint(rows)
+    return rows
 def main():
     print("hello")
     r = requests.get('http://vweb2.brevardclerk.us/Foreclosures/foreclosure_sales.html')
