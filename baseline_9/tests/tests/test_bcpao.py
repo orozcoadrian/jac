064eaeda1e44a9624e91fd7538d267176573d3dc
adrian orozco <orozcoadrian@gmail.com>
2014-11-14 22:16:21 -0500

    moved bcpao test code to new file test_bcpao. changed bcpao to return no acct id if the query returns more than one result. in those cases need to find a way to find a single result

---
+++ baseline_9/tests/tests/test_bcpao.py
@@ -0,0 +1,202 @@
+'''
+Created on Nov 14, 2014
+
+@author: Adrian
+'''
+import unittest
+import jac.bcpao
+import jac.bclerk
+import pprint
+
+class Test(unittest.TestCase):
+
+
+    def test_house(self):
+        # case_number                case_title                        foreclosure_sale_date    brevard clerk    count    address                                zip code    latest_amount_due    liens                        defendants    bcpao acct    frame code    latest market value total    total base area    sq feet    year built
+        # 05-2007-CA-030452-XXXX-XX    DEUTSCHE BANK VS ROLANDO PEREZ    06/04/2014                link            1        1123 FLOWER ST NW , PALM BAY 32907    32907        237,804.43            05-2007-CA-030452-XXXX-XX                 2807459
+
+        i=jac.bcpao.get_bcpaco_item('2807459')
+        #https://www.bcpao.us/asp/Show_parcel.asp?acct=2807459&gen=T&tax=T&bld=T&oth=T&sal=T&lnd=T&leg=T&GoWhere=real_search.asp&SearchBy=Owner
+        pprint.pprint(i)
+        self.assertEqual(i['address'] , '1123 FLOWER ST NW , PALM BAY 32907')
+        self.assertEqual(i['latest market value total'] , '$49,910')
+        self.assertEqual(i['zip_code'] , '32907')
+        self.assertEqual(i['frame code'] , '04')
+        self.assertEqual(i['year built'] , '1985')
+        # self.assertEqual(i['sq feet'] , '')
+        self.assertEqual(i['total base area'] , '1,173')
+        self.assertEqual(i['use code']['use_code'] , '110')
+        self.assertEqual(i['use code']['use_code_str'] , 'R-SINGLE FAMILY RESIDENCE')
+
+    def test_condo(self):
+        # case_number                case_title                        foreclosure_sale_date    brevard clerk    count    address                                        zip code    latest_amount_due    liens                        defendants    bcpao acct    frame code    latest market value total    total base area    sq feet    year built
+        # 05-2014-CA-013327-XXXX-XX    BANK AMERICA VS ERNEST FINNEY    06/05/2014                link            208        7667 N WICKHAM RD  1009, MELBOURNE 32940    32940        202,665.14            05-2014-CA-013327-XXXX-XX                 2630481                    $58,030                                        1,256    1990
+
+        i=jac.bcpao.get_bcpaco_item('2630481')
+        #https://www.bcpao.us/asp/Show_parcel.asp?acct=2630481&gen=T&tax=T&bld=T&oth=T&sal=T&lnd=T&leg=T&GoWhere=real_search.asp&SearchBy=Owner
+        pprint.pprint(i)
+        self.assertEqual(i['address'] , '7667 N WICKHAM RD  1009, MELBOURNE 32940')
+        self.assertEqual(i['latest market value total'] , '$83,000')
+        self.assertEqual(i['zip_code'] , '32940')
+        # self.assertEqual(i['frame code'] , '04')
+        #self.assertEqual(i['year built'] , '1990') # broken
+        #self.assertEqual(i['sq feet'] , '1,256') # broken
+        # self.assertEqual(i['total base area'] , '1,173')
+
+    def test_legal(self):
+#                       T  R  S  SUBID    BLK
+#         Parcel ID:    28-36-26-KN-02134.0-0028.00
+        #                     sub,                   lot, block,   pb,   pg
+        i=jac.bcpao.get_acct_by_legal(('PORT MALABAR UNIT 42', '28', '2134', '21','105', '26', '28', '35', '00'))
+        pprint.pprint(i)
+        self.assertEqual(i , '2807459')
+
+    def test_legal_2(self):
+        # LT 20 PB 21 PG 45 HIGH ACRES ESTATES UNIT NO 1 S 19 T 21 R 35 SUBID 25:
+        # {'subid': '25', 'subd': 'HIGH ACRES ESTATES UNIT NO 1', 'pb': '21', 'lt': '20', 'pg': '45', 'blk': None}
+        #                     sub,                           lot,  block,   pb,   pg
+        i=jac.bcpao.get_acct_by_legal(('HIGH ACRES ESTATES UNIT NO 1', '20', None, '21','45', '19', '21', '35', '25'))
+        pprint.pprint(i)
+        self.assertEqual(i , '2104215')
+
+    def test_bclerk_then_bcpao1(self):
+        legal_str = 'LT 3 BLK A PB 28 PG 2 COUNTRY LAKE ESTS S 1/2 OF S 30 T 24 R 36 SUBID 54'
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '') # used to return '2423677' correctly by luck. the query returns two items (north and south). i changed the code to return None if the query returns more than one item (very unlikely that the first one is the correct one) 
+
+    def test_bclerk_then_bcpao2(self):
+        legal_str='LT 36 PB 29 PG 46 SHERWOOD FOREST P.U.D. II, REPLAT OF STAGE ONE, TRACT A S 24 T 21 R 34 SUBID 05'
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2101315')
+
+    def test_bclerk_then_bcpao3(self):
+        legal_str='BLK 8S U 3103 NE 1/4 OF NE 1/4 EX E 50 FT & EX CYPRESS SPRINGS CONDO ORB 5620/2802 S 16 T 28 R 37 SUBID 00'
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        self.assertEqual(legal['blk'], '8S')
+        self.assertEqual(legal['s'], '16')
+        self.assertEqual(legal['r'], '37')
+        self.assertEqual(legal['t'], '28')
+        self.assertEqual(legal['subid'], '00')
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2864518')
+
+    def test_bclerk_then_bcpao4(self):
+        legal_str='LT 1928 PB 10 PG 21 U 401 WINSLOW RESERVE SUBD THE MERIDIAN, A CONDOMINIUM PH I ORB 5782/5772 S 26 T 24 R 37 SUBID 27'
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        self.assertEqual(legal['blk'], None)
+        self.assertEqual(legal['s'], '26')
+        self.assertEqual(legal['r'], '37')
+        self.assertEqual(legal['t'], '24')
+        self.assertEqual(legal['subid'], '27')
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+#         self.assertEqual(acct, '---')
+
+    def test_bclerk_then_bcpao5(self):
+        legal_str='BLK 283F U 706 N 7.5 FT OF S 632.5 FT OF LOT 2 CANAVERAL BAY CONDO PH VII ORB 2648/2338 S 22 T 24 R 37 SUBID 00'
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        self.assertEqual(legal['blk'], '283F')
+        self.assertEqual(legal['s'], '22')
+        self.assertEqual(legal['r'], '37')
+        self.assertEqual(legal['t'], '24')
+        self.assertEqual(legal['subid'], '00')
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2441816')
+
+    def test_bclerk_then_bcpao6(self):
+        legal_str='LT 7804 PB 29 PG 71 U 3 LA CITA SECTION 5 SWEET MAGNOLIA CONDO ORB 2732/2040 S 16 T 22 R 35 SUBID MR'
+#         Parcel ID:    22-35-16-MR-00000.0-0078.04
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2207771')
+
+    def test_bclerk_then_bcpao7(self):
+        legal_str='LT 5 BLK 66 PB 4 PG 12 INDIAN RIVER CITY, REVISED PLAT OF S 22 T 22 R 35 SUBID 75'
+#         Parcel ID:    22-35-16-MR-00000.0-0078.04
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+#         self.assertEqual(acct, '---') # this one fails because there's a bcpao account only for lot 4 not 5
+
+    def test_bclerk_then_bcpao8(self):
+        legal_str='BLK 750J U 1-205 PART OF NE 1/4 OF NE 1/4 AS DES BELLA VISTA CONDO PHASE I ORB 5595/8053 S 20 T 25 R 36 SUBID 00'
+#         Parcel ID:    22-35-16-MR-00000.0-0078.04
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2537717')
+
+    def test_bclerk_then_bcpao9(self):
+        legal_str='BLK 7797 U 259 E 150 FT OF N 170 FT OF NE 1/4 CASA VERDE CLUB CONDO PH V ORB 2625/2765 BLDG N S 05 T 25 R 36 SUBID 00'
+#         Parcel ID:    22-35-16-MR-00000.0-0078.04
+        legal=jac.bclerk.get_legal_from_str(legal_str)
+        print('legal='+str(legal))
+        acct=str(jac.bcpao.get_acct_by_legal((legal['subd'],legal['lt'],legal['blk'],legal['pb'],legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print(acct)
+        self.assertEqual(acct, '2503567')
+
+
+    def assertAcctFromLegal(self, legal_str, expectedAcct):
+        legal = jac.bclerk.get_legal_from_str(legal_str)
+        print 'legal=' + str(legal)
+        acct = str(jac.bcpao.get_acct_by_legal((legal['subd'], legal['lt'], legal['blk'], legal['pb'], legal['pg'], legal['s'], legal['t'], legal['r'], legal['subid'])))
+        print acct
+        self.assertEqual(acct, expectedAcct)
+
+    def test_bclerk_then_bcpao10(self):
+        self.assertAcctFromLegal('LT 14 BLK 3 PB 19 PG 58 MARLIN SUBD S 05 T 25 R 36 SUBID 52', '2504233')
+    def test_bclerk_then_bcpao11(self):
+        #self.assertAcctFromLegal('LT 16 BLK D PB 44 PG 23 VIERA CENTRAL PUD TRACT 12 UNIT 1 PARCELS 1-3, PHASE 2B S 16 T 26 R 36 SUBID RF', '---')
+        #self.assertAcctFromLegal('LT 6 PB 1 PG 164 FLORIDA INDIAN RIVER LAND CO PARCEL 1 FROM S QUARTER CNR BEING INTERSEC OF CENLN OF ST RD 9 S 33 T 28 R 37', '---')
+        #self.assertAcctFromLegal('LT 710 BLK 48 PB 3 PG 7 U 10C2 AVON BY THE SEA OCEAN PARK CONDO NORTH ORB 2024/745 S 23 T 24 R 37 SUBID CG', '---') # LT 7.10
+        #self.assertAcctFromLegal('LT 36 PB 26 PG 103 INDUSTRIAL PLAZA UNIT 1 TRACT C COMM @ INTERSEC OF N R/W OF ELLIS RD & CENTERLN OF E DR S 26 T 27 R 36 SUBID 75', '---')
+        self.assertAcctFromLegal('LT 95 PB 48 PG 69 EAGLE LAKE EAST PHASE 1 S 15 T 28 R 37 SUBID 81', '2859710')
+        #self.assertAcctFromLegal('LT 3 BLK G PB 10 PG 22 GOLF PARK SUBD S 20 FT S 04 T 28 R 37 SUBID 51', '---')
+        self.assertAcctFromLegal('LT 14 BLK 3 PB 19 PG 58 MARLIN SUBD S 05 T 25 R 36 SUBID 52', '2504233')
+#         self.assertAcctFromLegal('LT 6 PB 1 PG 164 FLORIDA INDIAN RIVER LAND CO PARCEL 1 FROM S QUARTER CNR BEING INTERSEC OF CENLN OF ST RD 9 S 33 T 28 R 37', '---')
+        self.assertAcctFromLegal('BLK 20U U A21 N 227.29 FT OF E 128 FT OF W 778 TREASURE COAST HARBOUR VILLAS CONDO ORB 5741/5541 S 31 T 24 R 37 SUBID 00', '2461077')
+
+    def test_bclerk_then_bcpao12(self):
+        self.assertAcctFromLegal('LT 14 BLK 180 PB 23 PG 53 PORT ST JOHN UNIT 6 COMM @ NW CNR S 21 T 23 R 35 SUBID JX', '') # should be 2322639, not blank, not 2306233 !!!
+
+    def test_one(self):
+        i=jac.bcpao.get_bcpaco_item('2724389')
+        pprint.pprint(i)
+        self.assertEqual(i['address'] , '800  BALLARD DR , MELBOURNE 32935')
+        self.assertEqual(i['latest market value total'] , '$76,150')
+        self.assertEqual(i['zip_code'] , '32935')
+        self.assertEqual(i['frame code'] , '03')
+        self.assertEqual(i['year built'] , '1951')
+        # self.assertEqual(i['sq feet'] , '1,256')
+        self.assertEqual(i['total base area'] , '1,272')
+
+    def test_2613083(self):
+        i=jac.bcpao.get_bcpaco_item('2613083')
+        pprint.pprint(i)
+        self.assertEqual(i['address'] , '320  LEE AVE , SATELLITE BEACH 32937')
+        self.assertEqual(i['latest market value total'] , '$150,850')
+        self.assertEqual(i['zip_code'] , '32937')
+        self.assertEqual(i['frame code'] , '03')
+        self.assertEqual(i['year built'] , '1974')
+        # self.assertEqual(i['sq feet'] , '1,256')
+        self.assertEqual(i['total base area'] , '1,545')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
